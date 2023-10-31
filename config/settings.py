e9407b752e4b841be6a31a9129b0a8291d5fb763
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-31 21:56:19 +0300

    Создано приложение users и модель User

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    """Класс Юзер"""
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+
+    tg_username = models.CharField(max_length=150, verbose_name='Telegram username')
+    tg_chat_id = models.IntegerField(unique=True, verbose_name='Telegram chat id', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def save(self, *args, **kwargs):
+        """Создание и хеширование пароля"""
+        self.set_password(self.password)
+        super().save(*args, **kwargs)
