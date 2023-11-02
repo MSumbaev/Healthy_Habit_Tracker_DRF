c638b2e5a035806760435adfb51ee68cd576fc49
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-11-02 20:59:02 +0300

    в users/views.py добавлены permission_classes

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
+from habit.permissions import IsOwner
 from users.models import User
 from users.serializers import UserSerializer
 
@@ -12,17 +14,21 @@ class UserCreateAPIView(generics.CreateAPIView):
 class UserListAPIView(generics.ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAdminUser]
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
 
 
 class UserDestroyAPIView(generics.DestroyAPIView):
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
