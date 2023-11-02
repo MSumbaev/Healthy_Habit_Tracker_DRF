823c1b81fbaf7e2d3818090684472fb28459b6a7
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-11-02 20:35:22 +0300

    добавлены валидаторы в HabitSerializer

--- habit/serializers.py
+++ habit/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from habit.models import Habit
+from habit.validators import RewardOrLinkedValidator, ExecutionDurationValidator, LinkedHabitsValidator, \
+    PleasantHabitValidator, PeriodValidator
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+        validators = [
+            RewardOrLinkedValidator(),
+            ExecutionDurationValidator(),
+            LinkedHabitsValidator(),
+            PleasantHabitValidator(),
+            PeriodValidator()
+        ]
